9da67831e803cad71afb93bd440a5cfaee3714a6
Marcin Gąsienica-Makowski <me.martiinii@gmail.com>
2025-06-05 23:57:01 +0200

    chore: add root package to files

--- bump.config.ts
+++ bump.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from "bumpp";
 import { globSync } from "tinyglobby";
 
 export default defineConfig({
-  files: globSync(["./packages/*/package.json", "./apps/*/package.json"], {
-    expandDirectories: false,
-  }),
+  files: globSync(
+    ["./packages/*/package.json", "./apps/*/package.json", "./package.json"],
+    {
+      expandDirectories: false,
+    },
+  ),
 });
