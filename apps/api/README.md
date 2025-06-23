e87d3d2691aece5a4ca34df44852efa17446fb49
Marcin Gąsienica-Makowski <me.martiinii@gmail.com>
2025-06-23 22:18:44 +0200

    feat: enhance API build process and CLI options; add README and update dependencies

---
+++ apps/api/README.md
@@ -0,0 +1,4 @@
+# Local SQL API
+
+This package contains only API of Local SQL.
+For more information, visit [Local SQL Github](https://github.com/martiinii/local-sql)
---
+++ apps/api/build.ts
@@ -0,0 +1,72 @@
+import { rm } from "node:fs/promises";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import { copyFiles, prettyPrintBunBuildArtifact } from "@local-sql/utils/cli";
+import chalk from "chalk";
+
+// Clean up previous build directory
+await rm("./build", {
+  recursive: true,
+  force: true,
+});
+
+// Build the API
+const result = await Bun.build({
+  entrypoints: ["./src/index.ts", "./src/cli.ts"],
+  outdir: "./build",
+  target: "node",
+  minify: {
+    syntax: true,
+    whitespace: true,
+  },
+  banner: "#!/usr/bin/env node",
+});
+
+for (const output of result.outputs) {
+  prettyPrintBunBuildArtifact(output);
+}
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Create package.json and README for the build
+type PackageJson = Record<"name" | "description" | "version", string>;
+const packageJson: PackageJson = await Bun.file(
+  path.join(__dirname, "./package.json"),
+).json();
+
+const packageJsonBuild = {
+  name: packageJson.name,
+  description: packageJson.description,
+  version: packageJson.version,
+  type: "module",
+  bin: {
+    "@local-sql/api": "./build/index.js",
+  },
+};
+
+const readmeFile = Bun.file(path.join(__dirname, "./README.md"));
+
+await Bun.write(
+  "./build/package.json",
+  JSON.stringify(packageJsonBuild, null, 2),
+);
+await Bun.write("./build/README.md", readmeFile);
+
+// Copy public assets
+await copyFiles({
+  pattern: "**/*",
+  outdir: "./build/public",
+  baseDir: "./public",
+  msgName: "public",
+});
+
+// Copy migrations
+await copyFiles({
+  pattern: "**/*",
+  outdir: "./build/src/db/migrations",
+  baseDir: "./src/db/migrations",
+  msgName: "migrations",
+});
+
+console.log(chalk.green("\nAPI Build completed successfully"));
