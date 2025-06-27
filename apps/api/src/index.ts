5bce660e2fe0a5ea8ca7994280697ad90fe1ea70
Marcin Gąsienica-Makowski <me.martiinii@gmail.com>
2025-06-27 22:26:09 +0200

    fix: replace @elysiajs/static with single readFile

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,3 +1,6 @@
+import { readFile } from "node:fs/promises";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 import { swagger } from "@elysiajs/swagger";
 import { LOCAL_SERVER_ID } from "@local-sql/db-types";
 import chalk from "chalk";
@@ -5,6 +8,7 @@ import { Elysia } from "elysia";
 import { adapter } from "./adapter";
 import { LOCAL_SERVER_PORT } from "./constants";
 import { migrateDatabase } from "./db/migrate-database";
+import { IS_BUNDLED } from "./lib/is-bundled";
 import { timeout } from "./lib/timeout-util";
 import { prettyLog } from "./plugins/pretty-log.plugin";
 import { setupPlugin } from "./plugins/setup.plugin";
@@ -12,10 +16,26 @@ import { initRouter } from "./routers/init.router";
 import { serverRouter } from "./routers/server.router";
 import { tokenRouter } from "./routers/token.router";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export const app = new Elysia({
   adapter: adapter,
 })
   .use(setupPlugin)
+  // Elysia built-in file and @elysiajs/static don't work with Node.js correctly
+  .get("/favicon.svg", async ({ set }) => {
+    const file = await readFile(
+      path.join(
+        __dirname,
+        IS_BUNDLED ? "./public" : "../public",
+        "favicon.svg",
+      ),
+    );
+    set.headers["content-type"] = "image/svg+xml";
+
+    return file;
+  })
   .use(
     swagger({
       path: "/swagger",
@@ -36,7 +56,7 @@ export const app = new Elysia({
         ],
       },
       scalarConfig: {
-        favicon: "/public/favicon.svg",
+        favicon: "favicon.svg",
       },
     }),
   )
