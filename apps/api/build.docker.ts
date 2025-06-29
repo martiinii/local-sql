7e7b3a904c43b791c786272f43bd6ec8021e90ec
Marcin Gąsienica-Makowski <me.martiinii@gmail.com>
2025-06-29 02:10:49 +0200

    feat: add Docker support

---
+++ apps/api/build.docker.ts
@@ -0,0 +1,52 @@
+import { rm } from "node:fs/promises";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import chalk from "chalk";
+
+// Clean up previous build directory
+await rm("./build-docker", {
+  recursive: true,
+  force: true,
+});
+
+// Build the API
+await Bun.$`FORCE_COLOR=1 bun build ./src/index.ts --compile --external "@libsql/*" --external libsql --minify-whitespace --minify-syntax --target bun --outfile ./build-docker/server --define 'process.env.IS_BUNDLED=true'`;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Create package.json and README for the build
+type PackageJson = Record<"name" | "description" | "version", string> &
+  Record<"dependencies", Record<string, string>>;
+const packageJson: PackageJson = await Bun.file(
+  path.join(__dirname, "./package.json"),
+).json();
+
+const extractDependenciesNames = ["@libsql/client"];
+const extractedDependencies = extractDependenciesNames.reduce(
+  (acc, cur) => {
+    if (packageJson.dependencies[cur]) {
+      acc[cur] = packageJson.dependencies[cur];
+    }
+
+    return acc;
+  },
+  {} as Record<string, string>,
+);
+
+const packageJsonBuild = {
+  name: packageJson.name,
+  description: packageJson.description,
+  version: packageJson.version,
+  type: "module",
+  bin: {
+    "@local-sql/api": "./cli.js",
+  },
+  dependencies: extractedDependencies,
+};
+
+await Bun.write(
+  "./build-docker/package.json",
+  JSON.stringify(packageJsonBuild, null, 2),
+);
+console.log(chalk.green("\nAPI Docker Build completed successfully"));
