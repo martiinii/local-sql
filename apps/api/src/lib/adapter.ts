96ea2983fc5ab6fa14058d0dfb98c35d72ace80e
Marcin Gąsienica-Makowski <me.martiinii@gmail.com>
2025-07-28 22:47:16 +0200

    fix(adapter): add type definition for ElysiaAdapter in adapter export

--- apps/api/src/lib/adapter.ts
+++ apps/api/src/lib/adapter.ts
@@ -1,5 +1,9 @@
 import node from "@elysiajs/node";
 import { detectRuntime } from "@local-sql/utils/detect-runtime";
+import type { ElysiaAdapter } from "elysia";
 
 const runtime = detectRuntime();
-export const adapter = runtime === "node" ? node() : undefined;
+
+// @ts-expect-error Casting node adapter to ElysiaAdapter causes some errors with missing "stop" property
+export const adapter: ElysiaAdapter | undefined =
+  runtime === "node" ? node() : undefined;
